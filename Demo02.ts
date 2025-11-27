bdb4659fab3a3beb27389a74fc5078fdc9aa2416
mean-stack-devhub-rohan0555 <rj636956@gmail.com>
2025-11-27 18:05:25 +0530

    files added

---
+++ Demo02.ts
@@ -0,0 +1,110 @@
+type valType = number | string | boolean;
+function printValue(val: valType) {
+  if (typeof val === "number") {
+    console.log("number is " + val);
+  } else if (typeof val === "string") {
+    console.log(`val is ${val}`);
+  } else {
+    console.log("type is unknown");
+  }
+}
+
+printValue(123);
+
+interface person {
+  name: string;
+  marks: number;
+}
+interface person {
+  age: number;
+  salary: number;
+}
+
+let val: person = {
+  name: "rohan",
+  age: 21,
+  salary: 55500,
+  marks: 600,
+};
+function getInfo(p: person) {
+  if ("salary" in val) {
+    console.log(`salary is ${p.salary}`);
+  } else if ("marks" in val) {
+    console.log(`marks are p.marks`);
+  }
+}
+
+getInfo(val);
+
+type cashPayment = { mode: "cash"; msg: string; amt: number };
+type upiPayment = { mode: "upi"; app: string };
+type cardPayment = { mode: "card"; type: string };
+
+type paymentMode = cashPayment | upiPayment | cardPayment;
+
+function processPayment(pay: paymentMode) {
+  if (pay.mode == "cash") {
+    console.log("payment is done in cash mode");
+    console.log(pay.msg);
+    console.log("amount transacted is " + pay.amt);
+  } else if (pay.mode == "upi") {
+    console.log("payment is done in upi mode");
+    console.log("platfom used is " + pay.app);
+  } else {
+    console.log("payment is done by card method");
+    console.log("type of card is " + pay.type);
+  }
+}
+processPayment({ mode: "cash", msg: "payment done through cash", amt: 5_55_000 });
+type successAPI = { status: "success"; data: string };
+type failedAPI = { status: "failure"; msg: string };
+type pendingAPI = { status: "pending"; msg: string };
+type apiHandling = successAPI | failedAPI | pendingAPI;
+
+function apiRes(res: apiHandling) {
+  if (res.status == "success") {
+    console.log(res.data);
+  } else if (res.status == "failure") {
+    console.log(res.msg);
+  } else if (res.status == "pending") {
+    console.log(res.msg);
+  }
+}
+
+apiRes({
+  status: "success",
+  data: "highest peak in the maharashtra is kalsubai peak",
+});
+apiRes({ status: "pending", msg: "hey currenly in pending status" });
+
+interface Product {
+  pid: number;
+  pName: string;
+  price: number;
+}
+
+interface UserResponse {
+  userId: number;
+  userName: string;
+  email: string;
+  age: number;
+}
+
+interface saveUser {
+  userId: string;
+  userName: string;
+  email: string;
+  age: number;
+}
+
+interface teacher {
+  tid: number;
+  tName: string;
+  salary: number;
+}
+
+interface course {
+  cid: number;
+  cName: string;
+  cFees: number;
+}
